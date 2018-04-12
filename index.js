6321000a62b3665b4f80de5446f0563abda16ed0
Rob LeCheminant <lecheminant@gmail.com>
2018-04-12 14:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function divide(a, b){
   return (a / b)
 }
 
+<<<<<<< HEAD
 function inc(a){
   a += 1
   return (a)
@@ -36,3 +37,17 @@ function preserveDecimal(n, a){
   parseFloat(a)
   return (a, n)
 }
+=======
+function inc(n){
+  a = n
+  n++
+  return (a)
+}
+
+function dec(n){
+  n = a
+  a--
+  return (a)
+}
+
+>>>>>>> 41081023932c68a0033c2dc3c9c635481ec69597
